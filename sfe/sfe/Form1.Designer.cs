808ce383fc1a0ecf175e240fefb55b92b2474926
Kintler11 <54033468+Kintler11@users.noreply.github.com>
2019-12-17 22:18:34 +0200

    Add files via upload

---
+++ sfe/sfe/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace sfe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(105, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Unlock Sandbox";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 58);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(91, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Game Beaten";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(62, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Light Ammo";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Shadow Quality";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Physics Updates";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 211);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(29, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(91, 17);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Unlock Arena";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(62, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Medium Ammo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Current Scene";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 252);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(105, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 301);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Boneworks SFE";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ sfe/sfe/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sfe
+{
+
+    public partial class Form1 : Form
+    {
+        int sandboxS;
+        static int hexadecimalToDecimal(String hexVal)
+        {
+            int len = hexVal.Length;
+
+            // Initializing base1 value  
+            // to 1, i.e 16^0 
+            int base1 = 1;
+
+            int dec_val = 0;
+
+            // Extracting characters as 
+            // digits from last character 
+            for (int i = len - 1; i >= 0; i--)
+            {
+                // if character lies in '0'-'9',  
+                // converting it to integral 0-9  
+                // by subtracting 48 from ASCII value 
+                if (hexVal[i] >= '0' &&
+                    hexVal[i] <= '9')
+                {
+                    dec_val += (hexVal[i] - 48) * base1;
+
+                    // incrementing base1 by power 
+                    base1 = base1 * 16;
+                }
+
+                // if character lies in 'A'-'F' ,  
+                // converting it to integral  
+                // 10 - 15 by subtracting 55  
+                // from ASCII value 
+                else if (hexVal[i] >= 'A' &&
+                         hexVal[i] <= 'F')
+                {
+                    dec_val += (hexVal[i] - 55) * base1;
+
+                    // incrementing base1 by power 
+                    base1 = base1 * 16;
+                }
+            }
+            return dec_val;
+        }
+        public void LoadSettings()
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryReader br = new BinaryReader(fl);
+
+            //Shadow Quality
+            br.BaseStream.Position = 0x3E1;
+            String text = hexadecimalToDecimal(br.ReadByte().ToString("X")).ToString();
+            br.Close();
+            textBox3.Text = text;
+
+
+            //Physics Update Rate
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            br.BaseStream.Position = 0x3A8;
+            string stext = hexadecimalToDecimal(br.ReadByte().ToString("X")).ToString();
+            br.Close();
+            textBox4.Text = stext;
+
+            //Read arena
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pInfo_00.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            string arenaName = null;
+            int c = 0;
+            for(int i = 0x414; i < 0x427; i++)
+            {
+                br.BaseStream.Position = i;
+
+                if (br.ReadByte().ToString("X2") == "00")
+                {
+                    sandboxS = i + 6;
+                    break;
+                }
+                if (br.ReadByte().ToString("X2") != "00")
+                {
+                arenaName += System.Convert.ToChar(System.Convert.ToUInt32(br.ReadByte().ToString("X2"), 16)).ToString();
+                }
+                c++;
+            }
+            br.Close();
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pInfo_00.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            br.BaseStream.Position = 0x415;
+            textBox5.Text = System.Convert.ToChar(System.Convert.ToUInt32(br.ReadByte().ToString("X2"), 16)).ToString() + arenaName;
+            br.Close();
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSettings();
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryReader br = new BinaryReader(fl);
+            //Read Sandbox
+            br.BaseStream.Position = 0x415;
+            string jeff = br.ReadByte().ToString("X2");
+                br.BaseStream.Position = sandboxS;
+                if (br.ReadByte().ToString("X2") == "01")
+                {
+                    br.Close();
+                    checkBox1.Checked = true;
+                }
+                else
+                {
+                    br.Close();
+                    checkBox1.Checked = false;
+                }
+                br.Close();
+
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            //Read Sandbox
+            br.BaseStream.Position = sandboxS - 1;
+            if (br.ReadByte().ToString("X2") == "01")
+            {
+                br.Close();
+                checkBox3.Checked = true;
+            }
+            else
+            {
+                br.Close();
+                checkBox3.Checked = false;
+            }
+            br.Close();
+
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            //Read Sandbox
+                br.BaseStream.Position = 0x40D;
+                if (br.ReadByte().ToString("X2") == "01")
+                {
+                    br.Close();
+                    checkBox2.Checked = true;
+                }
+                else
+                {
+                    br.Close();
+                    checkBox2.Checked = false;
+                }
+                br.Close();
+
+            fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\additional_resources1.dat", FileMode.Open);
+            br = new BinaryReader(fl);
+            //Read Ammo
+            string ammo = null;
+            int curAmmo = 0;
+
+            int runs = 0;
+            int runss = 0;
+
+            string ammoM = null;
+            int curAmmoM = 0;
+
+            int runsT = 0;
+            int runssT = 0;
+
+            int[] lAmmoloc = { 0x760, 0x75F, 0x75E, 0x72D, 0x72C, 0x72B, 0x6FE, 0x6FD, 0x6FC, 0x6C7, 0x6C6, 0x6C5, 0x694, 0x693, 0x692, 0x65E, 0x65D, 0x65C, 0x62E, 0x62D, 0x62C, 0x5FD, 0x5FC, 0x5FB, 0x5CD, 0x5CC, 0x5CB, 0x59A, 0x599, 0x598};
+            foreach(int ammoLoc in lAmmoloc)
+            {
+                runs++;
+                runss++;
+                try
+                {
+                    br.BaseStream.Position = ammoLoc;
+                    ammo += br.ReadByte().ToString("X2");
+                    if (runs == 3)
+                    {
+                        curAmmo += hexadecimalToDecimal(ammo);
+                        ammo = null;
+                        runs = 0;
+                    }
+
+                    textBox1.Text = curAmmo.ToString();
+                }
+                catch
+                {
+
+                }
+            }
+
+            foreach (int sammoLoc in lAmmoloc)
+            {
+                runsT++;
+                runssT++;
+                try
+                {
+                    br.BaseStream.Position = sammoLoc + 4;
+                    ammoM += br.ReadByte().ToString("X2");
+                    if (runsT == 3)
+                    {
+                        curAmmoM += hexadecimalToDecimal(ammoM);
+                        ammoM = null;
+                        runsT = 0;
+                    }
+
+                    textBox2.Text = curAmmoM.ToString();
+                }
+                catch
+                {
+
+                }
+            }
+            br.Close();
+            
+        }
+
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryReader br = new BinaryReader(fl);
+            BinaryWriter bw = new BinaryWriter(fl);
+            br.BaseStream.Position = 0x415;
+            string arena = br.ReadByte().ToString("X");
+            br.Close();
+                fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+                bw = new BinaryWriter(fl);
+                if (checkBox1.Checked == true)
+                {
+                    bw.BaseStream.Position = sandboxS;
+                    bw.Write((byte)01);
+                    bw.Close();
+                }
+                if (checkBox1.Checked == false)
+                {
+                    bw.BaseStream.Position = sandboxS;
+                    bw.Write((byte)00);
+                    bw.Close();
+                }
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryWriter bw = new BinaryWriter(fl);
+            bw.BaseStream.Position = 0x40D;
+            if (checkBox2.Checked == true)
+            {
+                bw.Write((byte)01);
+                bw.Close();
+            }
+
+            if (checkBox2.Checked == false)
+            {
+                bw.Write((byte)00);
+                bw.Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryWriter bw = new BinaryWriter(fl);
+            if((textBox3.Text != null) || (textBox3.Text != ""))
+            {
+                try
+                {
+                    int bit = int.Parse(textBox3.Text);
+                    bw.BaseStream.Position = 0x3E1;
+                    bw.Write((byte)bit);
+                    bw.Close();
+                }
+                catch
+                {
+                    bw.Close();
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryWriter bw = new BinaryWriter(fl);
+            if ((textBox3.Text != null) || (textBox3.Text != ""))
+            {
+                try
+                {
+                    int bit = int.Parse(textBox4.Text);
+                    bw.BaseStream.Position = 0x3A8;
+                    bw.Write((byte)bit);
+                    bw.Close();
+                }
+                catch
+                {
+                    bw.Close();
+                }
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            FileStream fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+            BinaryReader br = new BinaryReader(fl);
+            BinaryWriter bw = new BinaryWriter(fl);
+            br.BaseStream.Position = 0x415;
+            string arena = br.ReadByte().ToString("X");
+            br.Close();
+                fl = new FileStream(@"C:\Users\kaitt\AppData\LocalLow\Stress Level Zero\BONEWORKS\bw1_pinfo_00.dat", FileMode.Open);
+                bw = new BinaryWriter(fl);
+                if (checkBox3.Checked == true)
+                {
+                    bw.BaseStream.Position = sandboxS - 1;
+                    bw.Write((byte)01);
+                    bw.Close();
+                }
+                if (checkBox3.Checked == false)
+                {
+                    bw.BaseStream.Position = sandboxS - 1;
+                    bw.Write((byte)00);
+                    bw.Close();
+                }
+        }
+    }
+}
